d0a24f4b6638c2754a2f920343e9485e129757d3
vadyanZZZ <kulikoff.profanya@yandex.ru>
2021-08-02 21:03:37 +0300

    2 test

--- untitled/src/test/java/CarManagerTest.java
+++ untitled/src/test/java/CarManagerTest.java
@@ -3,6 +3,7 @@ import model.Color;
 import model.Size;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
 
@@ -15,5 +16,15 @@ public class CarManagerTest {
         Car car1 = carManager.generateCar(new Size(1,2), new Color(3,4,5));
 
         assertNotEquals(car, car1);
+    }
+
+    @Test
+    public void generateCar2() {
+        CarManager carManager = new CarManager();
+
+        Car car =  carManager.generateCar(new Size(1,1), new Color(1,1,1));
+        Car car1 =  carManager.generateCar(new Size(1,1), new Color(1,1,1));
+
+        assertEquals(1, 1);
     }
 }
